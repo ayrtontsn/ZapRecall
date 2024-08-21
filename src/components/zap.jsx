890da2ad652c49fefe1470ee03a95a0096f07a43
Ayrton Tales Silva do Nascimento <ayrtontsn@hotmail.com>
2024-08-21 16:17:16 -0300

    Feat: Adicionar cores nos botões de seleção de resposta e alterar o css do menu de pergunta inicial

--- src/components/zap.jsx
+++ src/components/zap.jsx
@@ -1,44 +1,148 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function Card({index,card}){
+export default function Card({ index, card }) {
 
-    const [Cor, setCor] = React.useState("")
-    const [Bol, setBol] = React.useState(true)
-    const [Carta, setCarta] = React.useState(flashcard(index,Cor,Bol))
+    const [Cor, setCor] = React.useState("");
+    const [Imagem, setImagem] = React.useState("src/assets/seta_play.png");
+    const [Bol, setBol] = React.useState(true);
+    const [Carta, setCarta] = React.useState(flashcard(index, Cor, Bol, Imagem));
 
-    function flashcard(index, Cor, Bol){
+    function flashcard(index, cor, Bol, img) {
         return (
-            <>
-                <div Cor={Cor}>Pergunta {index + 1}</div>
-                <img onClick={()=>Bol?setCarta(Zap(card)):""} src="src/assets/seta_play.png" alt="play" />
-            </>
+            <Container>
+                <Texto cor={cor} >Pergunta {index + 1}</Texto>
+                <img
+                onClick={() => Bol ? setCarta(Zap(card)) : ""}
+                src={img}
+                alt="play" />
+            </Container>
         )
     }
 
     function Zap(card) {
         return (
-            <>
-                <p1>{card.question}</p1>
+            <ContainerZap>
+                <p>{card.question}</p>
                 <img onClick={() => setCarta(Recall(card))} src="src/assets/seta_virar.png" alt="virar" />
-            </>
+            </ContainerZap>
         )
     }
 
     function Recall(card) {
         return (
-            <>
-                <p1>{card.answer}</p1>
+            <ContainerZap>
+                <p>{card.answer}</p>
                 <div>
-                    <img onClick={() => setCarta(flashcard(index, setCor("green"), setBol(false)))} src="src/assets/icone_certo.png" alt="certo" />
-                    <img onClick={() => setCarta(flashcard(index, setCor("red"), setBol(false)))} src="src/assets/icone_erro.png" alt="erro" />
-                    <img onClick={() => setCarta(flashcard(index, setCor("yellow"), setBol(false)))} src="src/assets/icone_quase.png" alt="quase" />
+                    <Botao
+                    onClick={() => (
+                        setCarta(flashcard(index, setCor("#FF3030"), setBol(false), setImagem("src/assets/icone_erro.png")))                        
+                    )}
+                    cor={"#FF3030"}
+                    >Não lembrei</Botao>
+                    <Botao
+                    onClick={() => (
+                        setCarta(flashcard(index, setCor("#FF922E"), setBol(false), setImagem("src/assets/icone_quase.png")))
+                    )}
+                    cor={"#FF922E"} >Quase não lembrei</Botao>
+                    <Botao
+                    onClick={() => (
+                        setCarta(flashcard(index, setCor("#2FBE34"), setBol(false),setImagem("src/assets/icone_certo.png")))                       
+                    )}
+                    cor={"#2FBE34"} >ZAP!</Botao>
                 </div>
-            </>
+            </ContainerZap>
         )
     }
 
-    return (   
+    return (
         <>
             {Carta}
         </>)
 }
+
+const Container = styled.div`
+
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    background: #Ffffff;
+    border-radius: 5px;
+    padding: 23px 10px;
+    margin: 25px 0;
+
+
+`;
+
+const Texto = styled.p`
+    font-family: Recursive;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 19.2px;
+    text-align: left;
+
+    color: ${props => props.cor ? props.cor: ""};
+    text-decoration: ${props => props.cor ? "line-through": ""};
+`
+
+const ContainerZap = styled.div`
+    width: 100%;
+    height: 130px;
+    
+    display: flex;
+
+    background: #FfffD4;
+    border-radius: 5px;
+    padding: 23px 10px;
+    margin: 25px 0;
+
+    position: relative;
+
+    p {
+        font-family: Recursive;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 21.6px;
+        text-align: left;
+
+    }
+    img {
+        display: flex;
+        align-items: end;
+        justify-content: end;
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+    }
+    div{
+        display: flex;
+        width: 95%;
+        justify-content: space-around;
+        margin: 10px 10px;
+        position: absolute;
+        bottom: 5%;
+        right: 0px;   
+    }
+`;
+
+
+
+
+
+const Botao = styled.button`
+    width: 30%;
+    height: 35px;
+    justify-content: space-between;
+
+    background-color: ${props => props.cor};
+    border-radius: 5px;
+
+    font-family: Recursive;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 14.4px;
+    text-align: center;
+    color:white;
+
+`;
